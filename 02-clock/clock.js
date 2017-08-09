75dbca5cc1f8a75d0d100ba4e8e83e5cbdf784c3
skate <ilagnev@gmail.com>
2017-08-09 15:59:27 +0300

    walking clock with some bugs 😒

--- 02-clock/clock.js
+++ 02-clock/clock.js
@@ -1,1 +1,20 @@
-// document.body.appendChild(document.createTextNode("start"));
+const hands = {
+    "seconds": document.querySelector('#seconds-hand'),
+    "minutes": document.querySelector('#minutes-hand'),
+    "hours": document.querySelector('#hours-hand'),
+};
+
+function clockTick() {
+    const now = new Date();
+
+    const secondsDegrees = (now.getSeconds() / 60 * 360) + 90;
+    hands.seconds.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const minutesDegrees = (now.getMinutes() / 60 * 360) + 90;
+    hands.minutes.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hoursDegrees = (now.getHours() / 12 * 360) + 90;
+    hands.hours.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+clockTick();
+setInterval(clockTick, 1000);
